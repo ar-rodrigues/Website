5d9030d87894490b10dc89685481979f385a0d5e
Alisson Rodrigues <68715629+ar-rodrigues@users.noreply.github.com>
2021-11-09 00:30:39 +0000

    added refer to requisites to show Language Choice

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ import { AiOutlineRight, AiOutlineLeft } from "react-icons/ai";
 
 function App() {
   const refer = document.referrer
-  console.log(refer)
   const [lang, setLang] = useState(0)
   
 
@@ -35,7 +34,7 @@ function App() {
     }
   }, [index, activeSection]);
   console.log(`lang is ${lang}`)
-  if(lang === 0)return <LanguageChoice setLang={setLang} />
+  if(lang === 0 && refer === false)return <LanguageChoice setLang={setLang} />
 
   return (
     <main>
