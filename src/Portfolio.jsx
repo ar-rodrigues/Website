4bb03867e6ef00707205069c5a46bbb424bbd902
ar-rodrigues <68715629+ar-rodrigues@users.noreply.github.com>
2023-06-10 07:31:20 +0000

    new item portfolio

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 function Portfolio() {
   const firstImg = "drakelfo-website.png";
   const secondgImg = "sites-preview.png";
+  const thirdImg = "dashboard.png";
   
 
   return (
@@ -26,6 +27,14 @@ function Portfolio() {
           href="https://alisson-rodrigues.netlify.app/"
           target="_blank">
             <img src={secondgImg} />
+        </a>
+      </div>
+       <div className="portf-item">
+        <h4>Fullstack Dashboard App - NEXT JS</h4>
+        <a 
+          href="https://drakelfo-dashboard.alissonrodrigu3.repl.co/dashboard"
+          target="_blank">
+            <img src={thirdImg} />
         </a>
       </div>
     </div>
